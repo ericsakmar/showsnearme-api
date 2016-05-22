972c4cff60741323e54a7340d4f193f7dff781da
Eric Sakmar <eric.sakmar@gmail.com>
2016-05-22 15:15:05 -0400

    Create Events * adds test * adds event creation

--- api/index.js
+++ api/index.js
@@ -2,9 +2,17 @@ var express = require('express'),
     router = express.Router(),
     Event = require('../models/event');
 
-router.get('/events', function(req, res, next) {
+router.get('/events', function(req, res) {
   Event.find(function (err, events) {
     res.json(events);
+  });
+});
+
+router.post('/events', function(req, res) {
+  const event = new Event(req.body);
+
+  event.save((err) => {
+    res.status(201).json(event);
   });
 });
 
