1b68b724e3e36c2ee3193a630e98e2e0c1595357
Eric Sakmar <eric.sakmar@gmail.com>
2016-12-26 18:50:04 -0500

    back to vagrant, for now

--- api/parser.js
+++ api/parser.js
@@ -2,7 +2,7 @@ const request = require('request-promise');
 
 function parse(id) {
   const options = {
-    uri: `http://parser:3000/${id}`,
+    uri: `http://localhost:3000/${id}`,
     json: true,
     headers: {
       'Content-Type':'application/json'
