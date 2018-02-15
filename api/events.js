56e6b2581ac0296c61154f5550a45011308f14ae
Eric Sakmar <eric.sakmar@gmail.com>
2018-02-14 22:23:34 -0500

    Adds Geo
    * filters by lat/lon
    * keeps description in db, but removes it from rest call
    * transforms incoming data
    * refactors

--- api/events.js
+++ api/events.js
@@ -1,41 +1,24 @@
 const locations = require('./locations');
 
 function toEventData(location, e) {
-  const evt = Object.assign({}, e, {
-    remoteId: e.id,
+  return Object.assign({}, e, {
     start_time: new Date(e.start_time),
   });
-  delete evt.id;
-  delete evt.place;
-
-  if (location) {
-    evt.location_id = location._id;
-  }
-
-  return evt;
 }
 
-function upsertEvent(db, location, eventData) {
-  return db.collection('events').findOneAndUpdate(
-    { remoteId:eventData.remoteId },
-    { $set:eventData },
-    { upsert:true, returnOriginal:false })
-
-      .then(res => Promise.resolve(res.value))
-
-      .then(event => Promise.resolve(Object.assign({}, event, {
-        place: location
-      })));
+function upsertEvent(db, eventData) {
+  return db
+    .collection('events')
+    .findOneAndUpdate(
+      {id: eventData.id},
+      {$set: eventData},
+      {upsert: true, returnOriginal: false}
+    )
+    .then(res => Promise.resolve(res.value));
 }
 
 function addEvent(db, e) {
-  if (e.place) {
-    return locations.upsertLocation(db, locations.toLocationData(e))
-      .then(location => upsertEvent(db, location, toEventData(location, e)));
-  }
-  else {
-    return upsertEvent(db, undefined, toEventData(undefined, e));
-  }
+  return upsertEvent(db, toEventData(undefined, e));
 }
 
 module.exports = {
